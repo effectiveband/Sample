033354439b1b533dfde5354a5d6e78bc8cbf25ee
Daniil Bugai <daniil.bugai@effective.band>
2022-02-24 03:04:10 +0600

    Move screen modules to screens folder, remove unused files

---
+++ screens/feed/src/main/java/com/github/daniilbug/feed/presentation/details/ArticleDetailsFragment.kt
@@ -0,0 +1,99 @@
+package com.github.daniilbug.feed.presentation.details
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.github.daniilbug.core.base.fragment.createFragmentWithFactory
+import com.github.daniilbug.core.base.viewBinding
+import com.github.daniilbug.core.base.viewmodel.BaseSavedStateViewModelFactory
+import com.github.daniilbug.core.extensions.parcelableArgs
+import com.github.daniilbug.feed.R
+import com.github.daniilbug.feed.databinding.FragmentArticleBinding
+import com.github.daniilbug.mainNavigation.ArticleDetails
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class ArticleDetailsFragment @Inject constructor(
+    private val viewModelCreator: ArticleDetailsViewModel.Creator
+) : Fragment(R.layout.fragment_article) {
+
+    companion object {
+        private const val DETAILS_KEY = "DETAILS_KEY"
+
+        fun withDetails(
+            factory: FragmentFactory,
+            articleDetails: ArticleDetails
+        ): ArticleDetailsFragment {
+            return createFragmentWithFactory<ArticleDetailsFragment>(factory).apply {
+                arguments = bundleOf(
+                    DETAILS_KEY to articleDetails
+                )
+            }
+        }
+    }
+
+    private val articleDetails: ArticleDetails by parcelableArgs(
+        DETAILS_KEY,
+        otherwise = { error("The fragment must be created with ${::withDetails.name}") }
+    )
+
+    private val viewModel: ArticleDetailsViewModel by viewModels {
+        BaseSavedStateViewModelFactory(this) {
+            viewModelCreator.create(articleDetails)
+        }
+    }
+
+    private val binding by viewBinding { FragmentArticleBinding.bind(it.requireView()) }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            articleOpenWebButton.setOnClickListener {
+                viewModel.sendEvent(ArticleDetailsEvent.OpenUrl)
+            }
+        }
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.state.collectLatest(::setState)
+            }
+        }
+    }
+
+    private fun setState(state: ArticleDetailsState) {
+        when (state) {
+            ArticleDetailsState.Loading -> setLoading()
+            is ArticleDetailsState.Article -> setArticle(state)
+        }
+    }
+
+    private fun setLoading() {
+        with(binding) {
+            articleScrollView.isVisible = false
+            articleProgressBar.isVisible = true
+        }
+    }
+
+    private fun setArticle(state: ArticleDetailsState.Article) {
+        with(binding) {
+            articleScrollView.isVisible = true
+            articleProgressBar.isVisible = false
+            articleImage.load(state.imageUrl)
+            articleTitle.text = state.title
+            articleContentText.text = state.content
+            val relativeDateString = DateUtils.getRelativeTimeSpanString(
+                state.date.time,
+                System.currentTimeMillis(),
+                DateUtils.DAY_IN_MILLIS
+            )
+            articleDateText.text = relativeDateString
+            articleSourceText.text = state.source
+        }
+    }
+}
---
+++ screens/search/src/main/java/com/github/daniilbug/search/presentation/adapter/SearchAdapter.kt
@@ -0,0 +1,87 @@
+package com.github.daniilbug.search.presentation.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.github.daniilbug.search.databinding.ItemSearchBinding
+import com.github.daniilbug.search.presentation.search.SearchItemUI
+
+class SearchAdapter(
+    private val onClick: OnClickListener
+) : PagingDataAdapter<SearchItemUI, SearchAdapter.ViewHolder>(DiffCallback) {
+
+    fun interface OnClickListener {
+        fun onSearchItemClick(item: SearchItemUI)
+    }
+
+    private object DiffCallback : DiffUtil.ItemCallback<SearchItemUI>() {
+        override fun areItemsTheSame(oldItem: SearchItemUI, newItem: SearchItemUI): Boolean {
+            return oldItem.title == newItem.title
+        }
+
+        override fun areContentsTheSame(oldItem: SearchItemUI, newItem: SearchItemUI): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemSearchBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding).apply {
+            binding.root.setOnClickListener {
+                getItem(bindingAdapterPosition)?.let(onClick::onSearchItemClick)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ViewHolder(
+        private val binding: ItemSearchBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: SearchItemUI?) {
+            with(binding) {
+                if (item == null) {
+                    bindPlaceholder()
+                } else {
+                    bindItem(item)
+                }
+            }
+        }
+
+        private fun ItemSearchBinding.bindPlaceholder() {
+            itemSearchImage.load(R.drawable.ic_image)
+            itemSearchTitle.text = "\t".repeat(Random.nextInt(10, 20))
+            itemSearchDescription.text = "\t".repeat(Random.nextInt(20, 60))
+            itemSearchDateText.text = "\t".repeat(Random.nextInt(10, 20))
+            itemSearchSourceText.text = "\t".repeat(Random.nextInt(10, 20))
+            val placeholderColor = root.context.themeColor(R.attr.colorSecondary)
+            itemSearchTitle.setBackgroundColor(placeholderColor)
+            itemSearchDescription.setBackgroundColor(placeholderColor)
+            itemSearchDateText.setBackgroundColor(placeholderColor)
+            itemSearchSourceText.setBackgroundColor(placeholderColor)
+        }
+
+        @SuppressLint("CheckResult")
+        private fun ItemSearchBinding.bindItem(item: SearchItemUI) {
+            itemSearchImage.load(item.imageUrl) {
+                this.placeholder(R.drawable.ic_image)
+                    .error(R.drawable.ic_image)
+            }
+            itemSearchTitle.text = item.title
+            itemSearchDescription.text = item.description
+            itemSearchSourceText.text = item.source
+            itemSearchDateText.text = DateFormat.getDateFormat(root.context).format(item.date)
+            itemSearchTitle.background = null
+            itemSearchDescription.background = null
+            itemSearchDateText.background = null
+            itemSearchSourceText.background = null
+        }
+    }
+}
